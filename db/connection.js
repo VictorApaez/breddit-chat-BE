821c7bc2d2f2ee494b4c01c129ae87b23809f4d4
Victor Apaez <vic.apaez@gmail.com>
2022-11-23 12:32:32 -0500

    fixed env

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
+const PORT = process.env.PROD_MONGODB || "mongodb://127.0.0.1:27017/chat";
 mongoose
-  .connect(process.env.MONGO_URL, {
+  .connect(PORT, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
